a53cf72438d95ad34f733694257264646ebf8520
Max Maurer <maxemanuel.maurer@gmail.com>
2014-08-21 15:43:08 +0200

    using env port if present

--- index.js
+++ index.js
@@ -58,6 +58,7 @@ socket.on('vote', function(option){
   });
 });
 
-http.listen(3000, function(){
+var port = process.env.PORT || 3000;
+http.listen(port, function(){
   console.log('listening on *:3000');
 });
